ee7f9b9e5fce0a79ac255e4a1da9f41b2d40c508
Kaveh Shahedi <kavehshahedi@gmail.com>
2024-08-21 09:50:13 -0400

    [UPDATE] Use multiple workers for communicating with the jpb jar file (i.e., concurrency)

--- jphb/services/java_service.py
+++ jphb/services/java_service.py
@@ -6,6 +6,7 @@ import json
 
 from jphb.services.similarity_service import SimilarityService
 
+from concurrent.futures import ThreadPoolExecutor
 
 class JavaService:
 
@@ -20,14 +21,15 @@ class JavaService:
 
     def __init__(self) -> None:
         self.jpb_path = os.path.join(sys.path[0], 'jphb', 'resources', 'jpb.jar')
+        self.executor = ThreadPoolExecutor(max_workers=8)
     
     def get_different_methods(self, first_code_path: str, second_code_path: str) -> Union[list[dict], None]:
         """
         This method gets two Java code snippets and returns the methods that are different between them.
         """
 
-        first_output = subprocess.check_output(['java', '-jar', self.jpb_path, '-get-methods-hash', first_code_path]).decode('utf-8').strip()
-        second_output = subprocess.check_output(['java', '-jar', self.jpb_path, '-get-methods-hash', second_code_path]).decode('utf-8').strip()
+        first_output = self.__execute_get_method_hashes(first_code_path)
+        second_output = self.__execute_get_method_hashes(second_code_path)
 
         if not first_output or not second_output:
             return None
@@ -77,12 +79,15 @@ class JavaService:
             - Input: 'public void com.example.A.method(int a, int b)'
             - Output: 'None-None-method-[int a, int b]'
         """
-
-        output = subprocess.check_output(['java', '-jar', self.jpb_path, '-convert-method-signature', method_signature])
-        output = output.decode('utf-8').strip()
-        if not output:
-            return None
         
-        return output
+        return self.executor.submit(self.__execute_convert_method_signature, method_signature).result()
+    
+    def __execute_get_method_hashes(self, code_path: str):
+        output = subprocess.check_output(['java', '-jar', self.jpb_path, '-get-methods-hash', code_path])
+        return output.decode('utf-8').strip() or None
+    
+    def __execute_convert_method_signature(self, method_signature: str):
+        output = subprocess.check_output(['java', '-jar', self.jpb_path, '-convert-method-signature', method_signature])
+        return output.decode('utf-8').strip() or None
     
     
